524aad6079859c03a43e6a18b4a02b0f9efdf624
Dzmitry Zakhar <dzahar@mail.ru>
2018-09-25 23:39:16 +0300

    7of17

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports =  function count(s, pairs) {
+module.exports = function count(s, pairs) {
 
   var n = 1, answer = 0, k = 0, counter = 0;
   var mask = s.split('');
@@ -6,14 +6,19 @@ module.exports =  function count(s, pairs) {
   for (let i = 0; i < pairs.length; i++) {
     n *= Math.pow(pairs[i][0], pairs[i][1]);
   }
-  if (n > 10000) return 0;
+  if (n > 1000000) return 0;
+    var j = 0;
   for (let i = 0; i <= n; i++){
-    if (i%2 ==0 && n%2 == 0 ||
-        i%3 ==0 && n%3 == 0 ||
-        i%5 ==0 && n%5 == 0 ||
-        i%7 ==0 && n%7 == 0) arr.push(0)
-    else arr.push (1);
+    if (findGCD(i+j,n) == 1){arr.push(1);}
+    if (findGCD(i+j,n) != 1){arr.push(0);}
+  /*  if (arr[arr.length - 1] == mask[j]){
+      j++;
+    }
+    if (j > mask.length){
+      j = 0;
+    }//*/
   }
+
   var res = arr.join('');
   var pos = -1;
   while ((pos = res.indexOf(s, pos+1)) != -1){
@@ -22,4 +27,16 @@ module.exports =  function count(s, pairs) {
   var result = answer % 1000000007;
   console.log(result);
   return result;
+}
+
+function findGCD(one, two){
+  var arr = [];
+  arr.push(one);
+  arr.push(two);
+  while((arr[0] != arr[1])){
+    arr.sort((a,b)=>a-b);
+    if(arr[0] == 0) break;
+    arr[1] = arr[1]%arr[0];
+  }
+  return arr[1];
 }
