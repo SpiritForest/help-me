4c16bb884dbd14980ab90ffd6161bc0b96636e1e
Dzmitry Zakhar <dzahar@mail.ru>
2018-09-26 01:36:57 +0300

    10of17

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
 module.exports = function count(s, pairs) {
 
-  var n = 1, answer = 0, k = 0, counter = 0;
-  var mask = s.split('');
+  var n = 1, answer = 0;
   var arr = [];
   for (let i = 0; i < pairs.length; i++) {
     n *= Math.pow(pairs[i][0], pairs[i][1]);
   }
-  if (n > 1000000) return 0;
+  if (n > 10000000) return 0;
 
-    var j = 1;
-  for (let i = 1; i <= n; i++){
-    if (findGCD(i+j,n) == 1){arr.push(1);}
-    if (findGCD(i+j,n) != 1){arr.push(0);}
-    if (arr[arr.length - 1] == mask[j]){j++;}
-    if (j == mask.length){j = 1;}
-   // console.log(j);
-  }
+for (let i = 0; i < n; i++){
+  if (findGCD(i,n) == 1){arr.push(1);}
+  else {arr.push(0);}
+}
 
   var res = arr.join('');
   var pos = -1;
   while ((pos = res.indexOf(s, pos+1)) != -1){
     answer++;
   }
+
   var result = answer % 1000000007;
-  //console.log(res);
-  //console.log(result);
   return result;
 }
 
 function findGCD(one, two){
-  var arr = [];
-  arr.push(one);
-  arr.push(two);
+  var arr = [one, two];
   while((arr[0] != arr[1])){
     arr.sort((a,b)=>a-b);
     if(arr[0] == 0) break;
