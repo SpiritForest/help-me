a56f0f4bad1164a973ae9c3e7702483a32676cb7
Dzmitry Zakhar <dzahar@mail.ru>
2018-09-27 12:41:30 +0300

    11of17

---
+++ test2.js
@@ -0,0 +1,36 @@
+function count(s, pairs) {
+
+  var n = 1, answer = 0;
+  var arr = [];
+  for (let i = 0; i < pairs.length; i++) {
+    n *= Math.pow(pairs[i][0], pairs[i][1]);
+  }
+  if (n > 1000000) return 0;
+
+for (let i = 0; i < n; i++){
+  if (findGCD(i,n) == 1){arr.push(1);}
+  else {arr.push(0);}
+}
+
+  var res = arr.join('');
+  var pos = -1;
+  while ((pos = res.indexOf(s, pos+1)) != -1){
+    answer++;
+  }
+
+  var result = answer % 1000000007;
+  console.log(res);
+  console.log(result);
+  return result;
+}
+
+function findGCD(one, two){
+  var arr = [one, two];
+  while((arr[0] != arr[1])){
+    arr.sort((a,b)=>a-b);
+    if(arr[0] == 0) break;
+    arr[1] = arr[1]%arr[0];
+  }
+  return arr[1];
+}
+count('11', [[5, 1], [23, 1], [19, 1]]);
--- test3.js
+++ test3.js
@@ -1,4 +1,4 @@
-module.exports = function count(s, pairs) {
+function count(s, pairs) {
 
   var n = 1, answer = 0;
   var arr = [], arr2 = [];
@@ -8,7 +8,7 @@ module.exports = function count(s, pairs) {
     arr2.push(pairs[i][0]);
   }
 
-if (n > 100000000)  return 0;
+//if (n > 100000000)  return 0;
 let j = 0;
 for (let i = 0; i < n; i++){
   if (arr2.some(elem => i % elem == 0)) arr.push(0);
@@ -22,5 +22,18 @@ for (let i = 0; i < n; i++){
   }
 
   const result = answer % 1000000007;
+  console.log(result);
   return result;
 }
+
+count('1', [
+      [11, 1],
+      [13, 1],
+      [23, 1],
+      [19, 1],
+      [2, 1],
+      [3, 1],
+      [7, 1],
+      [17, 1],
+      [29, 1],
+    ]);
